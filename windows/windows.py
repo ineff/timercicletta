39994a6e24c17b612d3786cf3c7cc3b9f67396b2
ineff <mossa@poisson.phc.unipi.it>
2014-02-19 12:15:14 +0100

    Cambiamenti finali alla programma: trasformate le funzioni per aggiornare l'orologio e l'allarme in metodi dei rispettivi oggetti+altri cambiamenti minimali.

--- windows/windows.py
+++ windows/windows.py
@@ -19,20 +19,17 @@ class ClockWindow(Gtk.Window):
    def __init__(self):
 
       Gtk.Window.__init__(self,title='Timer') # Call the Gtk.Window constructor 
-      hbox = Gtk.Box(spacing=6) # Create a container in which put the timer 
-      label = Gtk.Label('') # Create a label which will hold the current time
 
+      self.hbox = Gtk.Box(spacing=6) # Create a container in which put the timer 
+      self.label = Gtk.Label('') # Create a label which will hold the current time
       self.schedule = {'lu':[],'ma':[],'me':[],'gi':[],'ve':[],'sa':[],'do':[]} 
       # schedule is a dictionary containing for every day a field which contain 
       # the list of start time of programs
       # for the day
 
-      self.label = label # Add a reference to the label in the object
-      self.hbox = hbox # Add a reference to the box in the object
-      hbox.pack_start(label, True, True, 0) # Put the label inside the container hbox
+      self.hbox.pack_start(self.label, True, True, 0) # Put the label inside the container hbox
 
-
-      self.add(hbox) # Put the box inside the window
+      self.add(self.hbox) # Put the box inside the window
 
       self.alarm = AlarmWindow() # Create the alarm window
       # self.alarm.reset()
@@ -40,8 +37,8 @@ class ClockWindow(Gtk.Window):
 
       self.updateSchedule()
 
-      GLib.timeout_add_seconds(1,setTimeLabel,self) # Set the clock
-      GLib.timeout_add_seconds(60*60*24,self.updateSchedule,self) # Every day update the schedule
+      GLib.timeout_add_seconds(1,self.setTimeLabel) # Set the clock
+      GLib.timeout_add_seconds(60*60*24,self.updateSchedule) # Every day update the schedule
 
 
    def updateSchedule(self): # This function will update the schedule
@@ -57,6 +54,28 @@ class ClockWindow(Gtk.Window):
                                     filter(lambda x: x['start'][0] == today, listaProgs)) 
       
       return True
+
+   def setTimeLabel(self):
+
+      day = ['lu','ma','me','gi','ve','sa','do']
+      self.label.set_text(time.strftime('%H:%M:%S')) # Set the label to the current time
+      # Next we control if in five minutes start a new program.
+      now = time.localtime()
+      today = now.tm_wday
+      hour = now.tm_hour
+      minutes = now.tm_min
+      
+      timeNow = Timer(hour=hour,min=minutes)
+      
+      for index in range(0,5):
+         timeNow.incMin()
+
+      if timeNow in self.schedule[day[today]] and not self.alarm.get_property('visible'):
+         self.alarm.reset() # Reset the countdown of the self
+         self.alarm.start() # start the countdown
+      
+      return True
+      
       
 
 class AlarmWindow(Gtk.Window):
@@ -64,22 +83,13 @@ class AlarmWindow(Gtk.Window):
    def __init__(self):
 
       Gtk.Window.__init__(self,title='Alarm')
+      self.hbox = Gtk.Box(spacing=6)
+      self.label = Gtk.Label('')
+      self.countdown = Timer(hour=0,min=5,sec=0) # Add to the label a countdown
 
-#      self.__state__ = False # A boolean stating if the alarm is active
-
-      hbox = Gtk.Box(spacing=6)
-      label = Gtk.Label('')
-
-      countdown = Timer(hour=0,min=5,sec=0) # Add to the label a countdown
-
-      self.hbox = hbox
-      self.label = label
-      self.countdown = countdown
-
-      hbox.pack_start(label, True, True, 0)
-
+      self.hbox.pack_start(self.label, True, True, 0)
       self.set_name('Alarm')
-      self.add(hbox)
+      self.add(self.hbox)
 
    def reset(self): # Reset the counter
       self.label.countdown = Timer(hour=0,min=5,sec=0)
@@ -90,54 +100,27 @@ class AlarmWindow(Gtk.Window):
          return False 
       # Otherwise ...
       self.show_all()
-#      self.__state__ = True 
-      GLib.timeout_add_seconds(1,updateCountdown,self)      
+      GLib.timeout_add_seconds(1,self.updateCountdown)      
       return True
 
-# Here are some auxiliary functions used to update the windows.
+   def updateCountdown(self):
 
-def setTimeLabel(window): # Function which set a label to the current time
-
-   day = ['lu','ma','me','gi','ve','sa','do']
-   window.label.set_text(time.strftime('%H:%M:%S')) # Set the label to the current time
-   # Next we control if in five minutes start a new program.
-   now = time.localtime()
-   today = now.tm_wday
-   hour = now.tm_hour
-   minutes = now.tm_min
-
-   timeNow = Timer(hour=hour,min=minutes)
-
-   print(timeNow)
-   print(window.schedule[day[today]])
-
-   for index in range(0,5):
-      timeNow.incMin()
-
-   print(timeNow)
-
-   if timeNow in window.schedule[day[today]] and not window.alarm.get_property('visible'):
-      window.alarm.reset() # Reset the countdown of the window
-      window.alarm.start() # start the countdown
-      print('It worked!')
-   return True
-
-
-def updateCountdown(window):
-
-   flashTime = Timer(min=1,sec=0) # Time when start to flashing
-   finish = Timer() # Time is end
-
-   window.countdown.dec() # Decrement the countdown
-   window.label.set_text('- '+str(window.countdown))
-   if window.countdown < flashTime:# If we are up to 1 minute to the alarm
-      if window.label.get_visible(): # we toggle visible status of the label
-         window.label.hide()
-      else:
-         window.label.show()
-   if window.countdown == finish: # if countdown is zero, we stop cycling
-      window.hide()               # and make the window disappear
-#      window.__state__ = False    # put alarm in stopped mode
-      return False
-   else:
+      flashTime = Timer(min=1,sec=0) # Time when start to flashing
+      finish = Timer() # Time is end
+      
+      self.countdown.dec() # Decrement the countdown
+      self.label.set_text('- '+str(self.countdown))
+      if self.countdown < flashTime:# If we are up to 1 minute to the alarm
+         if self.label.get_visible(): # we toggle visible status of the label
+            self.label.hide()
+         else:
+            self.label.show()
+      if self.countdown == finish: # if countdown is zero, we stop cycling
+         self.hide()               # make the self disappear
+         return False # and stop the countdown
+   
       return True # Otherwise we countinue to cycle
+
+
+
+
